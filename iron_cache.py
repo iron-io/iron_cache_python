c480be5effef05a342ffe426b44154a3567a3e63
Paddy Foran <paddy@secondbit.org>
2012-05-10 09:33:35 -0400

    Updated to use latest iron_core API and be more object-oriented.

--- iron_cache.py
+++ iron_cache.py
@@ -1,107 +1,61 @@
-from iron_rest import *
-import ConfigParser
+import iron_core
+import urllib
+try:
+    import json
+except:
+    import simplejson as json
+
+
+class Item:
+    cache = None
+    key = None
+    value = None
+
+    def __str__(self):
+        if self.value is not None:
+            return self.value
+        if self.key is not None:
+            return "IronCache Item '%s'" % self.key
+        if self.cache is not None:
+            return "IronCache Item in Cache '%s'" % self.cache
+        return "IronCache item"
+
+    def __repr__(self):
+        if self.value is not None:
+            if self.cache is not None and self.key is not None:
+                return "%s[%s]: %s" % (self.cache, self.key, self.value)
+            elif self.cache is None:
+                return "%s: %s" % (self.key, self.value)
+            else:
+                return "%s" % self.value
+        else:
+            return "IronCache Item"
+
+    def __init__(self, values={}, **kwargs):
+        for k in kwargs.keys():
+            values[k] = kwargs[k]
+
+        for prop in values.keys():
+            if prop == "cache":
+                self.cache = values["cache"]
+            elif prop == "key":
+                self.key = values["key"]
+            elif prop == "value":
+                self.value = values["value"]
 
 class IronCache:
-    def __init__(self, project_id=None, token=None, protocol='https',
-            host='cache-aws-us-east-1.iron.io', port=443, version=1,
-            config=None):
+    NAME = "iron_cache_python"
+    VERSION = "0.1.0"
+
+    def __init__(self, **kwargs):
         """Prepare a configured instance of the API wrapper and return it.
 
-        Keyword arguments:
-        project_id -- The ID for the project, found on http://hud.iron.io.
-                      Defaults to None. project_id or config must be set 
-                      (with a project_id value in the config file) or a
-                      ValueError will be raised.
-        token -- The OAuth2 token that grants access to the project specified
-                 by project_id or in the config file. Found on
-                 http://hud.iron.io. Defaults to None. token or config must
-                 be set (with a token value in the config file) or a ValueError
-                 will be raised.
-        protocol -- The protocol to use for requests. Defaults to 'https'.
-                    Overrides the protocol specified in the config file, if
-                    config is set.
-        host -- The hostname of the API server. Defaults to
-                'cache-aws-us-east-1.iron.io'. Overrides the host specified in
-                the config file, if config is set.
-        port -- The port to connect to the API server on. Defaults to 443.
-                Overrides the port specified in the config file, if config is
-                set.
-        version -- The version of the API to use. Defaults to 1. Overrides the
-                   version specified in the config file, if config is set.
-        config -- The path to the config file to use when configuring the
-                  client. Should be a valid .ini file. Defaults to None. If
-                  None, both token and project_id must be set, or a ValueError
-                  will be thrown.
-        """
-        self.token = None
-        self.version = None
-        self.project_id = None
-        self.protocol = None
-        self.host = None
-        self.port = None
-        self.config = None
-        self.client = None
+        Keyword arguments are passed directly to iron_core_python; consult its
+        documentation for a full list and possible values."""
+        self.client = iron_core.IronClient(name=IronCache.NAME,
+                version=IronCache.VERSION, product="iron_cache", **kwargs)
 
-        if config is not None:
-            config_file = ConfigParser.RawConfigParser()
-            config_file.read(config)
-            try:
-                self.token = config_file.get("iron_cache", "token")
-            except ConfigParser.NoOptionError:
-                pass
-            try:
-                self.project_id = config_file.get("iron_cache", "project_id")
-            except ConfigParser.NoOptionError:
-                pass
-            try:
-                self.version = config_file.get("iron_cache", "version")
-            except ConfigParser.NoOptionError:
-                pass
-            try:
-                self.protocol = config_file.get("iron_cache", "protocol")
-            except ConfigParser.NoOptionError:
-                pass
-            try:
-                self.host = config_file.get("iron_cache", "host")
-            except ConfigParser.NoOptionError:
-                pass
-            try:
-                self.port = config_file.get("iron_cache", "port")
-            except ConfigParser.NoOptionError:
-                pass
-
-        if token is not None:
-            self.token = token
-        if project_id is not None:
-            self.project_id = project_id
-        if protocol is not None:
-            self.protocol = protocol
-        if host is not None:
-            self.host = host
-        if port is not None:
-            self.port = port
-        if version is not None:
-            self.version = version
-
-        if self.token is None:
-            raise ValueError("No token provided.")
-        if self.project_id is None:
-            raise ValueError("No project_id provided.")
-        if self.protocol is None:
-            raise ValueError("No protocol specified.")
-        if self.host is None:
-            raise ValueError("No host specified.")
-        if self.port is None:
-            raise ValueError("No port specified.")
-        if self.version is None:
-            raise ValueError("No API version specified.")
-
-        self.client = IronClient(name="iron_cache_python", version="0.1.0",
-                host=self.host, project_id=self.project_id, token=self.token,
-                protocol=self.protocol, port=self.port,
-                api_version=self.version)
-
-    def listCaches(self, options={}):
+    def caches(self, options={}):
         """Query the server for a list of caches, parse the JSON response, and
         return the result.
 
@@ -110,18 +64,98 @@ class IronCache:
                    http://dev.iron.io/cache/reference/api/#list_caches for more
                    information on defaults and possible values.
         """
+        query = urllib.urlencode(options)
+        url = "caches"
+        if query != "":
+            url = "%s?%s" % (url, query)
+        result = self.client.get(url)
+        result = json.loads(result["body"])
+        return [cache["name"] for cache in result]
 
-        return self.client.get("caches")
-
-    def getCache(self, cache, options={}):
-        """Query the server for info on a specific cache, parse the JSON, and
-        return the result.
+    def get(self, cache, key):
+        """Query the server for an item, parse the JSON, and return the result.
 
         Keyword arguments:
-        cache -- the name of the cache to be retrieved. Required.
+        cache -- the name of the cache that the item resides in. Required.
+        key -- the key of the item that you'd like to retrieve. Required.
+        """
+        cache = urllib.quote_plus(cache)
+        key = urllib.quote_plus(key)
+        url = "caches/%s/items/%s" % (cache, key)
+        result = self.client.get(url)
+        result = json.loads(result["body"])
+        return Item(values=result)
+
+    def put(self, cache, key, value, options={}):
+        """Query the server to set the key specified to the value specified in
+        the specified cache.
+
+        Keyword arguments:
+        cache -- the cache to store the item in. Required.        
+        key -- the name of the key to be set. Required.
+        value -- the value to set key to. Must be a string or JSON
+                 serialisable. Required.
         options -- a dict of arguments to send with the request. See
-                   http://dev.iron.io/cache/reference/api/#get_cache for more
+                   http://dev.iron.io/cache/reference/api/#put_item for more
                    information on defaults and possible values.
         """
+        options["body"] = value
+        body = json.dumps(options)
+        
+        cache = urllib.quote_plus(cache)
+        key = urllib.quote_plus(key)
 
-        return self.client.get("caches/%s" % cache)
+        result = self.client.put("caches/%s/items/%s" % (cache, key), body,
+                {"Content-Type": "application/json"})
+        return Item(cache=cache, key=key, value=value)
+
+
+    def delete(self, cache, key):
+        """Query the server to delete the key specified from the cache
+        specified.
+
+        Keyword arguments:
+        cache -- the cache to delete the item from. Required.
+        key -- the key the item is stored under. Required.
+        """
+
+        cache = urllib.quote_plus(cache)
+        key = urllib.quote_plus(key)
+
+        self.client.delete("caches/%s/items/%s" % (cache, key))
+
+        return True
+
+    def increment(self, cache, key, amount=1):
+        """Query the server to increment the value of the key by the specified
+        amount. Negative amounts can be used to decrement.
+
+        Keyword arguments:
+        cache -- the cache the item belongs to. Required.
+        key -- the key the item is stored under. Required.
+        amount -- the amount to increment the value by. Can be negative to
+                  decrement the value. Defaults to 1.
+        """
+
+        cache = urllib.quote_plus(cache)
+        key = urllib.quote_plus(key)
+
+        body = json.dumps({"amount": amount})
+
+        result = self.client.post("caches/%s/items/%s/increment" % (cache,
+            key), body, {"Content-Type": "application/json"})
+        result = json.loads(result["body"])
+        return Item(values=result, cache=cache, key=key)
+
+    def decrement(self, cache, key, amount=1):
+        """A convenience function for passing negative values to increment.
+
+        Keyword arguments:
+        cache -- the cache the item belongs to. Required.
+        key -- the key the item is stored under. Required.
+        amount -- the amount to increment the value by. Can be negative to
+                  decrement the value. Defaults to 1.
+        """
+        amount = amount * -1
+
+        return self.increment(cache, key, amount, options)
