f2b449d1c367426d2d1edd2e225cecee22651472
Paddy Foran <paddy@iron.io>
2012-07-20 20:32:02 -0400

    Added tests and a gitignore file.

---
+++ test.py
@@ -0,0 +1,42 @@
+from iron_cache import *
+import unittest
+import httplib
+import time
+
+
+class TestIronCache(unittest.TestCase):
+
+    def setUp(self):
+        self.cache = IronCache("test_cache")
+
+    def test_get(self):
+        self.cache.put("test_item", "testing")
+	item = self.cache.get("test_item")
+        self.assertEqual(item.value, "testing")
+
+    def test_delete(self):
+        self.cache.put("test_item", "will be deleted")
+	self.cache.delete("test_item")
+	self.assertRaises(httplib.HTTPException, self.cache.get,
+	        "test_item")
+
+    def test_increment(self):
+        self.cache.put("test_item", 2)
+	self.cache.increment("test_item")
+	item = self.cache.get("test_item")
+	self.assertEqual(item.value, 3)
+	self.cache.increment("test_item", amount=42)
+	item = self.cache.get("test_item")
+	self.assertEqual(item.value, 45)
+
+    def test_decrement(self):
+        self.cache.put("test_item", 100)
+	self.cache.decrement("test_item")
+	item = self.cache.get("test_item")
+	self.assertEqual(item.value, 99)
+	self.cache.decrement("test_item", amount=98)
+	item = self.cache.get("test_item")
+	self.assertEqual(item.value, 1)
+
+if __name__ == '__main__':
+    unittest.main()
