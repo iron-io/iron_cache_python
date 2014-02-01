96ecae896c3fd86c3cfe43ae8b309dc5069d9e1c
y-p <yoval@gmx.com>
2014-02-01 12:11:39 +0200

    BUG: long is not PY3 compat

--- iron_cache.py
+++ iron_cache.py
@@ -8,12 +8,10 @@ except:
 try:
     str_type = basestring
     int_types = (int, long)
-    quote_plus = urllib.quote_plus
 except NameError:
     # PY3
     str_type = str
     int_types = (int,)
-    quote_plus = urllib.parse.quote_plus
 
 class Item:
     cache = None
